973b9ea5627c31896c16ec40b7736415a28b8315
TKDchampion <as1219123@yahoo.com.tw>
2023-02-13 17:58:36 +0800

    router bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
+  const Demo = lazy(() => import('./Demo'));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<div>loading</div>}>
+      <div className="container-fluid appBox">
+        <Routes>
+          <Route path="/demo" element={<Demo />}></Route>
+          <Route path="/" element={<Navigate to="demo" />} />
+          <Route path="*" element={<div className="not-found">not found</div>} />
+        </Routes>
+      </div>
+    </Suspense>
   );
 }
 
